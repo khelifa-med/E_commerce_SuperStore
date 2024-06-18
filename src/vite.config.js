45327c494b71e471e9c820841d70797385bce730
Mohammed_ <mohammedkhelifa93@gmail.com>
2024-06-18 18:48:14 +0200

    Configure for GitHub Pages deployment2

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/khelifa-med/', // Replace 'your-repo-name' with the name of your repository
+  base: '/E_commerce_SuperStore/', // Replace with your repository name
 });
